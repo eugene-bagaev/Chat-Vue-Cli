0de106810c9b7e8309cafd7e6bf4b974802a9974
iminsite <eugene.bagaev@gmail.com>
2018-12-24 19:05:46 +0300

    Finished login process. Added events between chat, login, registration components. Added session control.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <v-app>
 
     <v-content v-if="state == 'chat'">
-        <Chat />
+        <Chat v-on:logout="logout" />
     </v-content>
 
     <v-content v-if="state == 'login'">
-        <Login />
+        <Login v-on:login="loginSuccess" v-on:signup="signup" />
     </v-content>
 
     <v-content v-if="state == 'reg'">
@@ -17,22 +17,47 @@
 </template>
 
 <script>
-import Login from './components/LoginForm'
-import Chat from './components/Chat'
-import Registration from './components/Registration'
+import Login            from './components/LoginForm';
+import Chat             from './components/Chat';
+import Registration     from './components/Registration';
 
 export default {
-  name: 'App',
-  components: {
+    name: 'App',
+    components: {
         Login,
         Chat,
         Registration
-  },
-  data () {
-    return {
-      state: 'reg'
+    },
+    data () {
+        return {
+            state: ''
+        }
+    },
+    methods: {
+        loginSuccess(value) {
+            if (value) {
+                this.state = 'chat';
+            }
+        },
+        logout(value) {
+            if (value) {
+                this.state = 'login'
+            }
+        },
+        signup(value) {
+            if (value) {
+                this.state = 'reg'
+            }
+        }
+    },
+    beforeMount() {
+        if (this.$session.exists()) {
+            console.log('Session exist');
+            this.state = 'chat'
+        } else {
+            this.state = 'login'
+        }
     }
-  }
 }
 // state for displaying different components
 </script>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -11,57 +11,140 @@
                                 <v-tooltip bottom>
                                     <v-btn
                                             slot="activator"
-                                            :href="source"
-                                            icon
-                                            large
+                                            @click="signup"
+                                            color="primary"
                                             target="_blank"
                                     >
-                                        <v-icon large>code</v-icon>
+                                        Sign up
                                     </v-btn>
-                                    <span>Info</span>
+                                    <span>Sign up</span>
                                 </v-tooltip>
                             </v-toolbar>
                             <v-card-text>
-                                <v-form>
-                                    <v-text-field prepend-icon="person" name="login" label="Login" type="text" required></v-text-field>
-                                    <v-text-field id="password" prepend-icon="lock" name="password" required label="Password" type="password"></v-text-field>
+                                <v-form ref="form">
+                                    <v-text-field prepend-icon="person"
+                                                  name="login"
+                                                  label="Login"
+                                                  v-model="login"
+                                                  :rules="loginRules"
+                                                  type="text"
+                                                  required></v-text-field>
+                                    <v-text-field id="password"
+                                                  prepend-icon="lock"
+                                                  v-model="password"
+                                                  name="password"
+                                                  required
+                                                  :rules="passwordRules"
+                                                  label="Password"
+                                                  type="password"></v-text-field>
                                 </v-form>
                             </v-card-text>
                             <v-card-actions>
                                 <!--<v-spacer></v-spacer>-->
-                                <v-checkbox label="Remember me" v-model="checkbox"></v-checkbox>
-                                <v-btn color="primary">Login</v-btn>
+                                <v-checkbox label="Remember me" v-model="remember"></v-checkbox>
+                                <v-btn color="primary" @click="submit">Login</v-btn>
                             </v-card-actions>
                         </v-card>
                     </v-flex>
                 </v-layout>
             </v-container>
         </v-content>
+        <v-snackbar
+                v-model="popupVisible"
+                :color="popupColor"
+                :top="popupTop"
+                :timeout="popupDelay"
+                :vertical="popupMode === 'vertical'">
+            {{ popupText }}
+            <v-btn dark flat @click="popupVisible = false">
+                Close
+            </v-btn>
+        </v-snackbar>
     </v-app>
 </template>
 
 <script>
-    var serverConnection = new WebSocket("ws://localhost:8080/chat");
+    import Vue          from 'vue';
+    import WebSocket    from 'vue-native-websocket';
+    import VueSession   from 'vue-session';
 
-    serverConnection.onopen = function () {
-        console.log('Connection Success');
-        // initUser();
-    };
+    Vue.use(WebSocket, "ws://localhost:8080/login");
+    Vue.use(VueSession);
 
     export default {
 
         data: () => ({
+            login: '',
+            password: '',
+            popupText: 'Hello',
+            popupColor: 'success',
+            popupDelay: 6000,
+            popupVisible : false,
+            popupTop : true,
+            popupMode: 'normal',
             drawer: null,
-            checkbox: false
+            remember: false,
+            passwordRules: [
+                p => !!p || 'Password is required.',
+                p => !(p.length <= 2) || 'Password must be longer than 2 characters.'
+            ],
+            loginRules: [
+                v => !!v || 'Login is required.',
+                v => !(v.length <= 2) || 'Login must be longer than 2 characters.'
+            ],
+            isError: false
         }),
         props: {
             source: String
         },
-        beforeCreate() {
-            console.log('Nothing gets called before me!')
+        methods: {
+            submit() {
+                if (this.$refs.form.validate()) {
+                    let loginInfo = {
+                        login       : this.login,
+                        password    : this.password,
+                        remember    : this.remember
+                    };
+                    this.$socket.send(JSON.stringify(loginInfo))
+                } else {
+                    this.showToast('Please check the entered data in the form', 'error');
+                }
+            },
+            loginLogic(serverData) {
+                if (!serverData.hasAccess) {
+                    this.loginUserError(serverData);
+                } else if (serverData.hasAccess && serverData) {
+                    this.loginSuccess(serverData);
+                }
+            },
+            loginSuccess(serverData) {
+                console.log(serverData);
+                this.showToast(serverData.status, 'success');
+                if (this.remember) { this.startSession(serverData); }
+                setTimeout(() => this.$emit('login', true), 1500);
+            },
+            loginUserError(serverData) {
+                this.showToast(serverData.status, 'error');
+            },
+            showToast(text, color) {
+                this.popupText = text;
+                this.popupColor = color
+                this.popupVisible = true;
+            },
+            startSession(serverData) {
+                console.log('Start Session');
+                this.$session.start();
+                this.$session.set('token', serverData.key);
+                this.$session.set('user', serverData.username);
+            },
+            signup() {
+                this.$emit('signup', true);
+            }
         },
-        beforeDestroy() {
-            serverConnection.close();
+        created: function() {
+            this.$options.sockets.onmessage = function (data) {
+                this.loginLogic(JSON.parse(data.data));
+            }
         }
     }
 </script>
