769549f7437a47f599529fac80ce6927e0564316
Eugene Bagaev <eugene.bagaev@vrpconsulting.com>
2019-01-16 12:39:33 +0300

    Changed IP server

--- src/components/WebSocket.vue
+++ src/components/WebSocket.vue
@@ -9,7 +9,7 @@
     import VueSession   from 'vue-session';
     import VueCookie    from 'vue-cookies';
 
-    Vue.use(WebSocket, "ws://localhost:8080/chatServer/chat_entry_point");
+    Vue.use(WebSocket, "ws://104.248.243.134:8080/chatServer/chat_entry_point");
     Vue.use(VueSession);
     Vue.use(VueCookie);
 
